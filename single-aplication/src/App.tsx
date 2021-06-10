f67299476d765f875391b6f80a6681a81da8f771
santiago leon <45574059+KKGoo@users.noreply.github.com>
2021-06-10 09:09:05 -0500

    [update]

--- single-aplication/src/App.tsx
+++ single-aplication/src/App.tsx
@@ -3,12 +3,14 @@ import "./styles/global.css"
 import logo from './logo.svg';
 import LogIn from './pages/login'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Register from './pages/register';
 
 function App() {
   return (
     <BrowserRouter>
         <Switch>
           <Route exact path="/" component={LogIn} />
+          <Route exact path="/register" component={Register} />
         </Switch>
     </BrowserRouter>
   );
---
+++ single-aplication/src/pages/register.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="Login">
+      <div className="Login__container">
+        <div className="backgrounds">
+          <h1>
+            Hello <br /> welcome <br /> back!
+          </h1>
+        </div>
+        <form className="Form__container" action="">
+          <h1 className="Form__header">LogIn</h1>
+          <div>
+            <div className="Form__input-container">
+              <p className="Form__input-label">username</p>
+              <input className="Form__input" type="text" />
+            </div>
+            <div className="Form__input-container">
+              <p className="Form__input-label">password</p>
+              <input className="Form__input" type="text" />
+            </div>
+            <div className="Form__checkbox-container">
+              <input className="Form__input-checkbox" type="checkbox" />
+              <p>Remember me</p>
+            </div>
+          </div>
+          <div className="Form__buttons">
+            <button className="Form__button">Sign in</button>
+           <Link to="/register">Sign up</Link> 
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
